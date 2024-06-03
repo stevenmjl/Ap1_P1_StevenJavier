1c2d05d69bba9ce7947c6e65b374c286e2e613b6
stevenmjl <stevenmjl.24@gmail.com>
2024-06-03 18:43:25 -0400

    Modelo Artículo completado.

--- Ap1_P1_StevenJavier/Models/Articulos.cs
+++ Ap1_P1_StevenJavier/Models/Articulos.cs
@@ -1,8 +1,25 @@
-﻿namespace Ap1_P1_StevenJavier.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ap1_P1_StevenJavier.Models
 {
     public class Articulos
     {
-        // ArticuloId, Descripcion, Costo, Ganancia, Precio 
+        [Key]
+        public int ArticulosId { get; set; }
 
+        [Required(ErrorMessage = "Nombre del artículo necesario.")]
+        public string? Descripcion { get; set; }
+        
+        [Required(ErrorMessage = "Valor necesario.")]
+        [Range(0.1, 1000000, ErrorMessage = "Número no permitido.")]
+        public double Costo { get; set; }
+        
+        [Required(ErrorMessage = "Valor necesario.")]
+        [Range(0.1, 1000000, ErrorMessage = "Número no permitido.")]
+        public double Ganancia { get; set; }
+        
+        [Required(ErrorMessage = "Valor necesario.")]
+        [Range(0.1, 1000000, ErrorMessage = "Número no permitido.")]
+        public double Precio { get; set; }
     }
 }
