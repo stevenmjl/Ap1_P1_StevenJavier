e7c805d33775f1742fab595f436a9f37a9553271
stevenmjl <stevenmjl.24@gmail.com>
2024-06-03 18:51:33 -0400

    Creando Contexto.

---
+++ Ap1_P1_StevenJavier/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Ap1_P1_StevenJavier.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Ap1_P1_StevenJavier.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Articulos> Articulos { get; set; }
+    }
+}
